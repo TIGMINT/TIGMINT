1654fb1ff8e2e0da86a19033852889ae39f1e80f
Yajas Sardana <yajassardana@gmail.com>
2020-06-26 23:17:29 +0530

    test python child scripts

---
+++ API server/script1.py
@@ -0,0 +1,4 @@
+import sys
+print('#Hello from python#')
+print('First param:'+sys.argv[1]+'#')
+print('Second param:'+sys.argv[2]+'#')
--- API server/server.js
+++ API server/server.js
@@ -1,19 +1,24 @@
-var express = require('express');
-var app = express();
-var spawn = require('child_process').spawn;
-
-app.get('/name', (req, res) => {
-	var process = spawn('python', [
-		'./hello.py',
-		req.query.firstname,
-		req.query.lastname,
-	]);
-
-	process.stdout.on('data', function (data) {
-		res.send(data.toString());
+const express = require('express');
+const { spawn } = require('child_process');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+	var dataToSend;
+	// spawn new child process to call the python script
+	const python = spawn('python', ['script1.py', 'node.js', 'python']);
+	// collect data from script
+	python.stdout.on('data', function (data) {
+		console.log('Pipe data from python script ...');
+		dataToSend = data.toString();
+		res.send(dataToSend);
+	});
+	// in close event we are sure that stream from child process is closed
+	python.on('close', (code) => {
+		console.log(`child process close all stdio with code ${code}`);
+		// send data to browser
 	});
 });
-
-app.listen(3000, function () {
-	console.log('server running on port 3000');
-});
+app.listen(port, () =>
+	console.log(`Example app listening on port 
+${port}!`)
+);
