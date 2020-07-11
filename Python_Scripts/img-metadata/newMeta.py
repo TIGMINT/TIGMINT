be2c4bde67a81a97bfd34451be27aadec9498ec6
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-07-11 12:15:22 +0530

    update metadata-analysis

---
+++ Python_Scripts/img-metadata/newMeta.py
@@ -0,0 +1,18 @@
+""" For linux the user must install exiftool using command sudo apt install exiftool """
+import platform
+import exiftool
+import json
+import os
+curentOS = platform.system()
+files = "bridge.jpg"
+
+with exiftool.ExifTool() as et:
+    if curentOS == "Windows":
+        et.executable = os.getcwd() + "\exiftool"
+    else:
+        et.executable = "exiftool"
+
+    metadata = et.get_metadata(files)
+print(metadata)
+with open('newMetaData.json', 'w') as fp:
+    json.dump(metadata, fp)
