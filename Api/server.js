736edad3aedc7c3020ebe6865278a6e19b9b46a3
Yajas Sardana <yajassardana@gmail.com>
2020-07-14 15:10:43 +0530

    documentation

--- Api/server.js
+++ Api/server.js
@@ -33,6 +33,9 @@ app.get('/twitteranalysis',(req,res)=>{
 app.get('/instagram',(req,res)=>{
 	res.render('instagram.ejs')
 })
+app.get('/documentation',(req,res)=>{
+	res.render('documentation.ejs')
+})
 // app.get('/test',(req,res)=>{
 // 	let coordinates = "28.617245604288797, 77.20818042755127"
 // 	res.render('twitterOutput.ejs',{data:coordinates});
