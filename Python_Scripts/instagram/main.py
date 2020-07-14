d140d7f69786124f55fee6fa44967b306fc3f636
Yajas Sardana <yajassardana@gmail.com>
2020-07-14 14:16:23 +0530

    downloadable story

--- Python_Scripts/instagram/main.py
+++ Python_Scripts/instagram/main.py
@@ -4,11 +4,5 @@ import sys
 
 username = sys.argv[1]
 
-# try:
 private = user_info.Username(username)
-# if private == False:
-#     insta_heighlight.main('_kaustubhsh')
-# else:
-#     print('Account is private, cannot retrive')
-# except:
-#     print("An exception occurred")
+# insta_heighlight.main(username)
