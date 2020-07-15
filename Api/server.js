84b0ad0479d9d6f12403704238ede7ef77358781
Kaustubh Sharma <kaustubhsharma97@gmail.com>
2020-07-15 18:54:27 +0530

    port update

--- Api/server.js
+++ Api/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+"/../public"));
 app.use(express.static(__dirname+"/../Python_Scripts/result"));
-const port = process.env.port || 8080;
+const port = process.env.port || 80;
 //* GET Routes
 app.get('/', (req, res) => {
 	res.render('index.ejs');
